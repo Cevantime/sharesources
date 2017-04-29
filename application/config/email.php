6e8f4d8f029b3b5ad0806583fda639d55afae9c6
alto971 <gwadaldesign@gmail.com>
2017-04-29 14:41:20 +0200

    ajout (temporaire) des fichiers non-versionnés

---
+++ application/config/email.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+$config['mailtype'] = 'html';
+
+$config['mailpath'] = "/usr/sbin/sendmail";
+$config['protocol'] = "smtp";
+$config['smtp_host'] = '';
+$config['smtp_port'] = "465";
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
+$config['charset'] = 'utf-8';
+$config['newline'] = "\r\n";
+$config['wordwrap'] = TRUE;
