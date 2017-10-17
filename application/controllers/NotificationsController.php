3789aee8c9892b804cd8716edd4ca4c03a583a69
Thibault Truffert <thibaulttruffert@hotmail.com>
2017-10-17 17:56:28 +0200

    fix de la redirection après édition des notifications

--- application/controllers/NotificationsController.php
+++ application/controllers/NotificationsController.php
@@ -72,7 +72,7 @@ class NotificationsController extends MY_Controller {
 
 			if ($this->notification->fromPost()) {
 				add_success('Le notification a bien été ajoutée');
-				redirect('notifications/index');
+				redirect('admin/notifications');
 			} else {
 				add_error($this->notification->getLastErrorsString());
 			}
