4a2369f4b259a6add3921410744a9aea3e68f14d
Thibault Truffert <thibaulttruffert@hotmail.com>
2017-11-26 16:33:20 +0100

    fix inline code display bug with pre tag (2)

--- assets_src/js/src/globals.js
+++ assets_src/js/src/globals.js
@@ -8,3 +8,12 @@ $('#minimizeSidebar').click(function(){
 	var miniActive = typeof md.misc.sidebar_mini_active === 'undefined' || ! md.misc.sidebar_mini_active;
 	$.ajax(window.baseURL+'userprefs/sidebar_collapsed/'+(miniActive ? 1 : 0));
 });
+
+if(hljs !== 'undefined'){
+  
+  $(document).ready(function() {
+    $('code.inline').each(function(i, block) {
+      hljs.highlightBlock(block);
+    });
+  });
+}
